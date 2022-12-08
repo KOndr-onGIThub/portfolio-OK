a65d900b0b98c1fa043e3b87ab35511e1fff431c
KOndr-onGIThub <kriska.ondrej@seznam.cz>
2022-12-08 19:59:19 +0100

    dokonceni CV

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -2,23 +2,91 @@
 
 @section('title','CV')
 
+@section('links')
+<link rel="stylesheet" href="css/cv.css" type="text/css">
+@endsection
+
 @section('content')
 
-    <header class="cv_heder">
+
+    <header class="cv_header">
         <h1>Ondřej Kriška</h1>
         <h3>junior developer PHP, JS, HTML</h3>
     </header>
     <address class="cv_contactList">
-        <a class="cv_phoneNo" href="tel:+420728697712">(+420) 728 697 712</a>
-        <a class="cv_mail" href="mailto:kriska.ondrej@seznam.cz">kriska.ondrej@seznam.cz</a>
+        <a class="cv_phoneNo" href="tel:+420728697712">
+            <i class="fa fa-phone"></i> (+420) 728 697 712
+
+        </a>
+        <a class="cv_mail" href="mailto:kriska.ondrej@seznam.cz">
+            <i class="fa fa-envelope"></i> kriska.ondrej@seznam.cz
+        </a>
+        
         <a class="cv_linkedin" href="https://www.linkedin.com/in/ond%C5%99ej-kri%C5%A1ka-928ab1254/" 
         target="_blank" rel="noopener">
-            linkedin
+        <i class="fa fa-linkedin"></i> linkedin
         </a>
-
     </address>
 
+    <section class="cv_perex">
+        
+        <img src="img/cv_ProfileFoto_preview.jpg" alt="profile photo for cv">
+        
+        <p>
+            Programování mě částečně provází celým dosavadním životem, ač jsem dosud na vyloženě programátorské pozici nepracoval. Jednoduché HTML stránky jsem tvořil již na škole a poté v prvním zaměstnání z dlouhé chvíle jako pomocný nástroj pro starší kolegy bez znalosti PC.
+            Nedávno se mi naskytla příležitost napsat PHP aplikaci, kterou jsem dříve vytvořil ve VBA v Excelu a která již nedostávala svým požadavkům.
+            Od té doby je pro mě programování nejen pracovní cíl.
+        </p>
+    </section>
 
+    <article>
+        <h3>Praxe:</h3>
+        <h5>Specialista interní logistiky / Programátor</h5>
+        <h6>2005 - současnost     Toyota Motor Manufacturing Czech Republic s.r.o.</h6>
+        <p>
+            Mou hlavní náplní je/bylo plánování a řízení toku materiálu 
+            interní logistikou a zavádění nových aplikac9 a systémů do procesů.
+            V r. 2020 jsem dostal možnost <strong>naprogramovat PHP aplikaci</strong>, 
+            která dopomůže k hladkému přechodu výroby na nový model aut. 
+            A to díly tomu, že umožní mnohonásobně rychlejší a přesnější 
+            řízení dodávek materiálu v krizových situacích.
+            <br>
+            Aplikace se trvale využívá nadále.
+            <br>
+            Pracoval jsme na ní kompletně sám jen s občasnou podporou od zkušeného kolegy z IS.
+        </p>
+    </article>
+
+    <article>
+        <h3>Odkazy na repo:</h3>
+
+            <ul>
+                <li><a title="link to GitHub" href="https://github.com/KOndr-onGIThub/HCMS">HCMS</a>(Hot Call Management system)</li>
+                <li><a title="link within this page" href="/">Portfilio</a></li>
+            </ul>
+
+    </article>
+
+    <article>
+        <h3>Další dovednosti:</h3>
+        <h5>Jazyky</h5>
+        <ul>
+            <li> Angličtina B1-B2</li>
+        </ul>
+
+        <h5>Ostatní</h5>
+        <ul>
+            <li>
+                Zkušenosti mám z výrobního i office prostředí nadnárodní společnosti Toyota. (Projekty, Kaizen, Just in Time atp.)
+            </li>
+        </ul>
+    </article>
+
+    <article>
+        <h3>Vzdělání:</h3>
+        <h5>DELTA – střední škola informatiky a ekonomie, obor Podnikání a ekonomie</h5> 
+        <h6>2015 - 2020     Zakončeno maturitní zkouškou</h6>
+    </article>
 
 
 
--- resources/views/layout_main.blade.php
+++ resources/views/layout_main.blade.php
@@ -13,10 +13,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="css/layoutStyles.css" type="text/css">
-    <link rel="stylesheet" href="fonts\fontawesome-free-6.2.1-web\css\fontawesome.min.css">
+    {{-- <link rel="stylesheet" href="fonts\fontawesome-free-6.2.1-web\css\fontawesome.min.css"> --}}
     <link rel="stylesheet" href="fonts\fontawesome-free-6.2.1-web\css\all.css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    
     <!-- fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    @yield('links')
 
     <!-- JS -->
     <script src="js/side_bar.js"></script>
@@ -27,7 +31,7 @@
 </head>
 <body>
     <div class="main_wraper">
-        <sidebar class="side_bar">
+        <aside class="side_bar">
             <header class="boder_heder">
             <a href="/" class="no_underline">
                 <img src="img/business_man_icon_grey.png" alt="avatar" />
@@ -37,28 +41,41 @@
 
             <nav class="side_nav">
                 <ul>
-                    <li><a href="#1" class="no_underline">Domů</a></li>
+                    <li><a href="#1" class="no_underline">
+                            <i class="fa fa-home fa-fw" aria-hidden="true"></i>Domů
+                        </a>
+                    </li>
                     <li><a href="/cv" class="no_underline">Životopis (CV)</a></li>
-                    <li><a href="#3" class="no_underline"></a></li>
+                    <li><a href="#3" class="no_underline"></a><i class="fa fa-twitter"></i></li>
                     <li><a href="#4" class="no_underline"></a></li>
                 </ul>
             </nav>       
-        </sidebar>
-        <asid class="aside_bar">
+        </aside>
+        <aside class="aside_bar">
             <div class="skryj" title="zobrazit/skrýt menu">
                 <div id="chevron_right"><i class="fa fa-chevron-right"></i></div>
                 <div id="chevron_left" hidden><i class="fa fa-chevron-left"></i></div>
             </div>
-        </asid> 
+        </aside> 
         
         <main class="mine_content">
             @yield('content', 'žádný obsah nenalezen')
         </main>
+
     </div>
     <footer class="flex_child">
-        <span class="act-date">Dnes je: {{$dateTime}} || </span>
-        <span>Poslední aktualizace z  {{env('APP_LAST_UPDATE', 'viz .env')}} na verzi {{env('APP_VERSION', 'viz .env')}} || </span>
-        <span class="versions-info">Postaveno na Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</span>
+        <div class="act-date">
+            <span>Dnes je:</span>
+            <span>{{$dateTime}}</span>
+        </div>
+        <div class="app-ver">
+            <span>Poslední aktualizace této stránky:</span>
+            <span>{{env('APP_LAST_UPDATE', 'viz .env')}} na verzi {{env('APP_VERSION', 'viz .env')}}</span>
+        </div>
+        <div class="versions-info">
+            <span>Postaveno na Laravel:</span>
+            <span>v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</span>
+        </div>
 
     </footer>
 </body>
