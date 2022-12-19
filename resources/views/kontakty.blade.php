4dd0bee4579dea22f42e7889eebe05c83021ca50
KOndr-onGIThub <kriska.ondrej@seznam.cz>
2022-12-19 20:34:12 +0100

    vytvoreni pohledu pro kontakty vcetne controlleru a csv

---
+++ resources/views/kontakty.blade.php
@@ -0,0 +1,17 @@
+@extends('layout_main')
+
+@section('title',config('app.shortName')  . '-Kontakty')
+
+@section('links')
+<link rel="stylesheet" href={{ asset('css/kontakty.css').'?'.env('APP_VERSION')}} type="text/css">
+
+@endsection
+
+@section('content')
+
+
+Obsah čeká na vytvoření
+
+
+
+@endsection
